9f974c230ab46a099d019f79ae5d34f378227040
肖 <603331487@qq.com>
2022-03-13 18:34:45 +0800

    不使用vite编译 使用tsc

--- types/src/useContextWithEqual.d.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from 'use-context-selector';
-/**
- * 比较相同contexthook 判断返回数据是否有变化进行渲染
- *
- * @example
- *
- * const {...} = useEqualContext(context, (v) => v.state);
- */
-export declare function useContextWithEqual<T, R>(context: Context<T>, selector: (state: T) => R): R;
--- types/src/useParams.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 获取路由参数
- *
- * @example
- *
- * const {id} = useParams<{id: string}>();
- *
- */
-export declare function useParams<T extends Record<string, string>>(): Partial<T>;
