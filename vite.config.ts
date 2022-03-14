43552313520e04c2cc52ca8256a0ce4974e69f26
肖 <603331487@qq.com>
2022-03-14 11:32:15 +0800

    使用vite进行库模式打包

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+/*
+ * @Description:
+ * @FilePath: /proste-taro-hooks/vite.config.ts
+ */
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import ts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [react(), ts({ outputDir: 'types' })],
+  build: {
+    outDir: '.',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: '@proste-taro/hooks',
+      fileName: format => `${format}/index.js`,
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', '@tarojs/taro', 'lodash', 'react-use', 'use-context-selector'],
+    },
+  },
+});
